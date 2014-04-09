fc1aea3b7687f22ed9e7bcc35e1ef3815d5326f5
Steffen Müller <steffen@mllrsohn.com>
2014-04-09 23:50:18 +0200

    x

--- test.sh
+++ test.sh
@@ -1,13 +1,3 @@
 sudo start redis
 sleep 10
-createuser -s yournamehere -U postgres
-echo "whhho"
-# @todo: setup integraion test
-# psql -c "CREATE USER screenspeak WITH PASSWORD 'screenspeak'" -U postgres
-# psql -c 'create database screenspeak-test;' -U postgres
-# psql -c 'GRANT ALL PRIVILEGES ON DATABASE screenspeak-test to screenspeak;' -U postgres
-# sleep 2
-# sqitch deploy ci
-# sudo start redis
-# sleep 10
-
+psql -c 'create database test;' -U postgres
