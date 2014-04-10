bff76501bc2e78f413d79854f9a79dea1740a44e
Steffen Müller <steffen@mllrsohn.com>
2014-04-10 11:16:26 +0200

    x

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 sudo start redis
 psql -h 127.0.0.1 -U postgres << EOF
     DROP ROLE IF EXISTS testuser;
-    CREATE ROLE testuser WITH ENCRYPTED PASSWORD 'testpass';
-    ALTER ROLE testuser WITH SUPERUSER;
-    ALTER ROLE testuser WITH LOGIN;
-    CREATE DATABASE test-db WITH OWNER=testuser;
-    GRANT ALL ON DATABASE test-db TO testuser;
+    CREATE ROLE 'testuser' WITH ENCRYPTED PASSWORD 'testpass';
+    ALTER ROLE 'testuser' WITH SUPERUSER;
+    ALTER ROLE 'testuser' WITH LOGIN;
+    CREATE DATABASE 'test-db' WITH OWNER=testuser;
+    GRANT ALL ON DATABASE 'test-db' TO testuser;
 EOF
