eb718d4d99107763c29602fa0d3fb5b3b1d9797f
Steffen Müller <steffen@mllrsohn.com>
2014-04-10 11:15:45 +0200

    x

--- test.sh
+++ test.sh
@@ -1,13 +1,9 @@
 sudo start redis
-until su ubuntu -c "psql -h 127.0.0.1 -U postgres -q <<-EOF
+psql -h 127.0.0.1 -U postgres << EOF
     DROP ROLE IF EXISTS testuser;
     CREATE ROLE testuser WITH ENCRYPTED PASSWORD 'testpass';
     ALTER ROLE testuser WITH SUPERUSER;
     ALTER ROLE testuser WITH LOGIN;
     CREATE DATABASE test-db WITH OWNER=testuser;
-    GRANT ALL ON DATABASE test-db TO testuser
-    EOF"; do
-    echo 'Unable to create the superuser.'
-    echo 'Retrying in 3 seconds...'
-    sleep 3
-done
+    GRANT ALL ON DATABASE test-db TO testuser;
+EOF
